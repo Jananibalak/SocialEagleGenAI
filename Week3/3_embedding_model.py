49a5c7bd192acec1d99af756105f9931265808da
Jananibalak <jananibala2512@gmail.com>
2026-02-15 16:50:44 +0530

    Initial clean commit

--- Week3/3_embedding_model.py
+++ Week3/3_embedding_model.py
@@ -2,7 +2,7 @@
 from langchain_openai import OpenAIEmbeddings
 
 embeddings = OpenAIEmbeddings(
-    model="openai/text-embedding-3-small" ,api_key="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" # cheap + good
+    model="openai/text-embedding-3-small" ,api_key="*************" # cheap + good
 )
 
 vector = embeddings.embed_query("LangChain is powerful")
